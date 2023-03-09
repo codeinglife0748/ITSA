808766e45328dcc10be6524ab2f956d03120fc1c
codeinglife0748 <4a9g0110@stust.edu.tw>
2023-03-09 15:20:08 +0800

    MPH to KMH

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -1,6 +1,12 @@
-#include<iostream>
+#include <iostream>  
+#include<iomanip>  
 using namespace std;
-int main() {
-
-	cout << "Hello There";
+int main()
+{
+    int a;
+    double b;
+    cin >> a;
+    b = a * 1.6;
+    cout << fixed << setprecision(1) << b << endl;
+    return 0;
 }
