916dc8afbaaba85ca3b168cf081be5084d6d691c
codeinglife0748 <4a9g0110@stust.edu.tw>
2023-03-15 15:10:07 +0800

    In_circle_or_not

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -1,6 +1,18 @@
-#include<iostream>
-using namespace std;
-int main() {
+#include <iostream>  
+#include <math.h>  
 
-	cout << "Hello There";
+using namespace std;
+int main()
+{
+    double x, y;
+    cin >> x >> y;
+    double d = x * x + y * y;
+    if (pow(d, 0.5) <= 100)
+    {
+        cout << "inside" << endl;
+    }
+    else {
+        cout << "outside" << endl;
+    }
+    return 0;
 }
