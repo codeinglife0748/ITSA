84c0199f7a6a766d9860cca7cf31cda949578daa
codeinglife0748 <4a9g0110@stust.edu.tw>
2023-03-23 15:33:59 +0800

    Prime Number

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -1,11 +1,27 @@
-#define _CRT_SECURE_NO_DEPRECATE
-#include<iostream>
-#include<iomanip>
-#include<math.h>
-#include<stdio.h>
-#include<stdlib.h>
+#define _CRT_SECURE_NO_DEPRECATE  
+#include<iostream>  
+#include<iomanip>  
+#include<math.h>  
+#include<stdio.h>  
+#include<stdlib.h>  
 using namespace std;
 int main() {
+    int a, i = 2;
+    cin >> a;
 
-	cout << "Hello There";
+    while (i < a)
+    {
+        i++;
+        if (a % i == 0)
+        {
+            break;
+        }
+    }
+    if (i == a)
+        cout << "YES" << endl;
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
 }
