61bb2dd5b88fe3a65ce097833861f8a6cdab22af
codeinglife0748 <4a9g0110@stust.edu.tw>
2023-03-31 21:47:26 +0800

    [C_AR05-易] 最少派車數

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -4,8 +4,29 @@
 #include<math.h>
 #include<stdio.h>
 #include<stdlib.h>
+#include<algorithm>
 using namespace std;
-int main() {
 
-	cout << "Hello There";
+int main()
+{
+
+	int s[25] = {}, b = 0;
+	int l = 0, a, n = 0;
+	fill(s, s + 25, 0);
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> l >> a;
+		for (int j = l; j < a; j++)
+		{
+			s[j]++;
+		}
+	}
+	for (int i = 0; i < 24; i++)
+	{
+		if (s[i] > b)
+		{
+			b = s[i];
+		}
+	}
+	cout << b << endl;
 }
