d946100d56379faee447b2c502ab1f3e1bc33b57
codeinglife0748 <4a9g0110@stust.edu.tw>
2023-04-19 23:47:26 +0800

    ITSA 23

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -4,8 +4,39 @@
 #include<math.h>
 #include<stdio.h>
 #include<stdlib.h>
+#include<sstream>
 using namespace std;
-int main() {
+void replaceD(string& str)
+{
+	int i = 0;
+	while (i < str.length())
+	{
+		if (str[i] == ',')
+			str[i] = ' ';
+		i++;
+	}
+}
 
-	cout << "Hello There";
+int main()
+{
+	string str;
+	int N, a1, a2, a3, cost, x, y, z;
+	N = a1 = a2 = a3 = cost = 0;
+	getline(cin, str);
+	replaceD(str);
+	stringstream ss(str);
+	ss >> N >> a1 >> a2 >> a3;
+	cost = a1 * 15 + a2 * 20 + a3 * 30;
+	z = (N - cost) / 50;
+	y = ((N - cost) % 50) / 5;
+	x = ((N - cost) % 50) % 5;
+	if (cost > N)
+	{
+		cout << "0" << endl;
+	}
+	else
+	{
+		cout << x << "," << y << "," << z << endl;
+	}
+	return 0;
 }
