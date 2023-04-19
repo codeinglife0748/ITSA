060096920e189a2674c8c4ff262cda7e752cc798
codeinglife0748 <4a9g0110@stust.edu.tw>
2023-04-19 23:40:11 +0800

    ITSA 19

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -5,7 +5,25 @@
 #include<stdio.h>
 #include<stdlib.h>
 using namespace std;
-int main() {
-
-	cout << "Hello There";
+int main()
+{
+	int n, s, d, max;
+	int time[25] = { 0 };
+	cin >> n;
+	max = 0;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> s >> d;
+		for (int j = s; j < d; j++)
+		{
+			time[j]++;
+		}
+		for (int k = 0; k < 25; k++)
+		{
+			if (time[k] > max)
+				max = time[k];
+		}
+	}
+	cout << max << endl;
+	return 0;
 }
