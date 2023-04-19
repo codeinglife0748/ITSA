bf45bd6ce93db2d75f888cef8bc3d90873e95e5a
codeinglife0748 <4a9g0110@stust.edu.tw>
2023-04-19 22:59:51 +0800

    TImes ITSA 15

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -5,7 +5,24 @@
 #include<stdio.h>
 #include<stdlib.h>
 using namespace std;
-int main() {
 
-	cout << "Hello There";
+	int appearNum(string & s, int p, string & l);
+	int main()
+	{
+		string inputS, inputL, save;
+
+		cin >> inputS >> inputL;
+		cout << appearNum(inputS, 0, inputL) << endl;
+		return 0;
+	}
+	int appearNum(string & s, int p, string & l)//字串遞迴(檢索字串，起始位置，被檢索字串) 
+	{
+		int a = 0;
+		if (p > l.length())return a = 0;//若偏移長度大於被檢索字串長度則回傳0 
+		for (int i = 0; i < s.length(); i++)//依檢索字串長度進行迴圈 
+		{
+			if (l[i + p] == s[i])continue;//若比對符合，則持續比對至完全符合 
+			else return a += appearNum(s, p + 1, l);//比對不符回傳當前符合次數 
+		}
+		a++; return a += appearNum(s, p + 1, l);
 }
