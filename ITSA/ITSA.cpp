f73380ffbb497c85c21c3d704f0e30bb30684f7f
codeinglife0748 <4a9g0110@stust.edu.tw>
2023-04-19 23:49:20 +0800

    ITSA 24

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -5,7 +5,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 using namespace std;
-int main() {
+int main()
+{
+	long double r, sum;
+	long int n, p;
+	cin >> r;
+	cin >> n;
+	cin >> p;
 
-	cout << "Hello There";
+	sum = 0;
+	while (n > 0)
+	{
+		sum += p;
+		sum *= (1.0 + r);
+		n--;
+	}
+	cout << (long long)sum << endl;
+	return 0;
 }
