afb18a7ad46c5bca40af3fccf968b71cb91b8b42
codeinglife0748 <4a9g0110@stust.edu.tw>
2023-04-19 23:33:10 +0800

    TIMES ITSA 15

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -3,26 +3,28 @@
 #include<iomanip>
 #include<math.h>
 #include<stdio.h>
+#include <vector>
+#include <string>
+#include <string.h> 
 #include<stdlib.h>
+#include<algorithm>
 using namespace std;
 
-	int appearNum(string & s, int p, string & l);
-	int main()
-	{
-		string inputS, inputL, save;
 
-		cin >> inputS >> inputL;
-		cout << appearNum(inputS, 0, inputL) << endl;
-		return 0;
+int main()
+{
+	string str;
+	getline(cin, str);
+
+	transform(str.begin(), str.end(), str.begin(), ::tolower);
+
+	int cnt[256] = {};
+	for (int i = 0; i < str.size(); i++) {
+		cnt[(int)str[i]]++;
 	}
-	int appearNum(string & s, int p, string & l)//字串遞迴(檢索字串，起始位置，被檢索字串) 
-	{
-		int a = 0;
-		if (p > l.length())return a = 0;//若偏移長度大於被檢索字串長度則回傳0 
-		for (int i = 0; i < s.length(); i++)//依檢索字串長度進行迴圈 
-		{
-			if (l[i + p] == s[i])continue;//若比對符合，則持續比對至完全符合 
-			else return a += appearNum(s, p + 1, l);//比對不符回傳當前符合次數 
+	for (int i = 0; i < 256; i++) {
+		if (cnt[i] != 0) {
+			cout << (char)i << ": " << cnt[i] << endl;
 		}
-		a++; return a += appearNum(s, p + 1, l);
+	}
 }
