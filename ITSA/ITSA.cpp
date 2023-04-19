c0c92ce82702d8f01b38aca88e2b5ed91591a1df
codeinglife0748 <4a9g0110@stust.edu.tw>
2023-04-19 23:43:20 +0800

    ITSA 17

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -4,8 +4,38 @@
 #include<math.h>
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+#include<ctype.h>  
 using namespace std;
 int main() {
-
-	cout << "Hello There";
+    char enter[10000];
+    fgets(enter, 9999, stdin);
+    int len = strlen(enter);
+    for (int i = 0; i < len; i++) {
+        enter[i] = tolower(enter[i]);
+    }
+    char ans[1000][1000];
+    int nowAns = 0;
+    char* pch = strtok(enter, " \r\n");
+    while (pch != NULL) {
+        int judge = 1;
+        for (int i = 0; i < nowAns; i++) {
+            if (strcmp(ans[i], pch) == 0) {
+                judge = 0;
+                break;
+            }
+        }
+        if (judge) {
+            strcpy(ans[nowAns], pch);
+            nowAns++;
+        }
+        pch = strtok(NULL, " \r\n");
+    }
+    for (int i = 0; i < nowAns; i++) {
+        if (i)
+            printf(" ");
+        printf("%s", ans[i]);
+    }
+    printf("\n");
+    return 0;
 }
